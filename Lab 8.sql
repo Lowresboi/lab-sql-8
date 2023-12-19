2b0ecbc39e95bb505e514c4d25136a98b0d7f19c
alex asensi <asensiphoto@gmail.com>
2023-12-19 11:06:20 +0000

    Second try I edited the last 3 queries

--- Lab 8.sql
+++ Lab 8.sql
@@ -11,23 +11,28 @@ FROM film
 WHERE length IS NOT NULL AND length > 0;
 
 -- 3. Number of films for each category
-SELECT c.name AS category, COUNT(fc.film_id) AS number_films_category
-FROM category c
-JOIN film_category fc ON c.category_id = fc.category_id
-GROUP BY c.name;
+SELECT name AS category, COUNT(film_id) AS number_films_category
+FROM category AS a
+JOIN film_category AS b
+ON a.category_id = b.category_id
+GROUP BY name;
 
 -- 4. Actor with the most appearances
-SELECT a.actor_id, a.first_name, a.last_name, COUNT(fa.film_id) AS number_films_appeared
-FROM actor a
-JOIN film_actor fa ON a.actor_id = fa.actor_id
-GROUP BY a.actor_id, a.first_name, a.last_name
-ORDER BY num_films_appeared DESC
+SELECT a.actor_id, a.first_name, a.last_name, COUNT(film_id) AS num_films_appeared
+FROM actor AS a
+JOIN film_actor AS b 
+ON a.actor_id = b.actor_id
+GROUP BY actor_id, first_name, last_name
+ORDER BY num_films_appeared
+DESC
 LIMIT 1;
 
 -- 5. Most active customer
 SELECT c.customer_id, c.first_name, c.last_name, COUNT(r.rental_id) AS customer_rentals
-FROM customer c
-JOIN rental r ON c.customer_id = r.customer_id
+FROM customer AS c
+JOIN rental AS r 
+ON c.customer_id = r.customer_id
 GROUP BY c.customer_id, c.first_name, c.last_name
-ORDER BY customer_rentals DESC
+ORDER BY customer_rentals 
+DESC
 LIMIT 1;
